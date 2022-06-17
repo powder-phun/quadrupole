c20232a915c3b5a15dbd00ef8a5d5bbb515cdbe3
Jakub <jendryka.j@gmail.com>
2022-06-17 15:41:53 +0200

    Finished working on comboboxes

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QComboBox
 from ui.main_window import Ui_MainWindow
+
+from parameter import ParameterID
 from utils import FLOAT_VALIDATOR
+
 
 class GUI(QMainWindow):
     def __init__(self):
@@ -10,7 +13,7 @@ class GUI(QMainWindow):
         self.main = None
         self.sweepOneSelected = None
         self.sweepTwoSelected = None
-    
+
     def initialize(self, main):
         self.main = main
         self.ui.paramDock.fill(self.main.params)
@@ -20,9 +23,12 @@ class GUI(QMainWindow):
         self.ui.sweepTwoMinEdit.setValidator(FLOAT_VALIDATOR)
         self.ui.sweepTwoMaxEdit.setValidator(FLOAT_VALIDATOR)
 
+        # Setting default sweep parameters so that they can be disabled in other menus
         self.sweepOneSelected = self.main.params[0].id
         self.sweepTwoSelected = self.main.params[1].id
+
         self.fill_comboboxes()
+        # Disabling sweep one parameter in paramdock (only sweep one enabled by default)
         self.ui.paramDock.set_enabled(self.sweepOneSelected, False)
 
         self.connect_all()
@@ -38,49 +44,84 @@ class GUI(QMainWindow):
     def connect_all(self):
         self.ui.sweepOneCheckbox.stateChanged.connect(self.set_enabled_sweep_one)
         self.ui.sweepTwoCheckbox.stateChanged.connect(self.set_enabled_sweep_two)
-        self.ui.sweepOneCombobox.currentIndexChanged.connect(self.combobox_one_index_changed)
-        self.ui.sweepTwoCombobox.currentIndexChanged.connect(self.combobox_two_index_changed)
+        self.ui.sweepOneCombobox.currentIndexChanged.connect(
+            self.combobox_one_index_changed
+        )
+        self.ui.sweepTwoCombobox.currentIndexChanged.connect(
+            self.combobox_two_index_changed
+        )
 
     def set_enabled_sweep_one(self, value):
+        # Enabling everything connected with sweep one
         self.ui.sweepOneCombobox.setEnabled(value)
         self.ui.sweepOneMinEdit.setEnabled(value)
         self.ui.sweepOneMaxEdit.setEnabled(value)
         self.ui.sweepOneStepsSpinbox.setEnabled(value)
+
+        # Enabling second sweep checkbox (it cannot be used without first sweep)
         self.ui.sweepTwoCheckbox.setEnabled(value)
+
+        # Reseting second sweep to disabled
         self.ui.sweepTwoCheckbox.setChecked(False)
+
+        # Disabling first sweep parameter in paramdock
         self.ui.paramDock.set_enabled(self.sweepOneSelected, not value)
+
+        # If disabling first sweep disable also the second one
         if not value:
             self.set_enabled_sweep_two(False)
             self.ui.paramDock.set_enabled(self.sweepTwoSelected, True)
 
     def set_enabled_sweep_two(self, value):
+        # Enabling everything connected with sweep two
         self.ui.sweepTwoCombobox.setEnabled(value)
         self.ui.sweepTwoMinEdit.setEnabled(value)
         self.ui.sweepTwoMaxEdit.setEnabled(value)
         self.ui.sweepTwoStepsSpinbox.setEnabled(value)
+
+        # Disabling first sweep parameter in paramdock
         self.ui.paramDock.set_enabled(self.sweepTwoSelected, not value)
 
-
     def combobox_one_index_changed(self, value):
-        text = self.ui.sweepOneCombobox.itemText(value)
-        identifier = next(param.id for param in self.main.params if param.name==text)
-        if identifier != self.sweepOneSelected:
-            self.ui.paramDock.set_enabled(identifier, False)
+        ret = self.process_combobox(
+            value,
+            self.ui.sweepOneCombobox,
+            self.ui.sweepTwoCombobox,
+            self.sweepOneSelected,
+        )
+        if ret is not None:
+            self.sweepOneSelected = ret
 
-            self.ui.sweepTwoCombobox.removeItem(self.ui.sweepTwoCombobox.findText(text))  # Remove parameter from other combobox
-            self.ui.sweepTwoCombobox.addItem(next(param.name for param in self.main.params if param.id==self.sweepOneSelected)) # Restore previously removed parameter from other combobox
-
-            self.ui.paramDock.set_enabled(self.sweepOneSelected, True)
-            self.sweepOneSelected = identifier
-    
     def combobox_two_index_changed(self, value):
-        text = self.ui.sweepTwoCombobox.itemText(value)
-        identifier = next(param.id for param in self.main.params if param.name==text)
-        if identifier != self.sweepTwoSelected:
+        ret = self.process_combobox(
+            value,
+            self.ui.sweepTwoCombobox,
+            self.ui.sweepOneCombobox,
+            self.sweepTwoSelected,
+        )
+        if ret is not None:
+            self.sweepTwoSelected = ret
+
+    def process_combobox(
+        self, value: int, changed: QComboBox, other: QComboBox, previous: ParameterID
+    ):
+        # Getting selected parameter info
+        text = changed.itemText(value)
+        identifier = next(param.id for param in self.main.params if param.name == text)
+
+        # Do only if there was a real change of parameter
+        if identifier != previous:
+            # Disabling sweep parameter in paramdock and restoring previous one
             self.ui.paramDock.set_enabled(identifier, False)
+            self.ui.paramDock.set_enabled(previous, True)
 
-            self.ui.sweepOneCombobox.removeItem(self.ui.sweepOneCombobox.findText(text))  # Remove parameter from other combobox
-            self.ui.sweepOneCombobox.addItem(next(param.name for param in self.main.params if param.id==self.sweepTwoSelected)) # Restore previously removed parameter from other combobox
+            # Removing selected parameter from other sweep combobox and restoring previous one
+            other.removeItem(other.findText(text))
+            other.addItem(
+                next(param.name for param in self.main.params if param.id == previous)
+            )
 
-            self.ui.paramDock.set_enabled(self.sweepTwoSelected, True)
-            self.sweepTwoSelected = identifier
+            # Saving selected parameter
+            return identifier
+        else:
+            return None
--- main.py
+++ main.py
@@ -5,27 +5,32 @@ from gui import GUI
 from controller import Controller
 from parameter import ParameterID, Parameter
 
+
 class Main:
     def __init__(self):
         self.app = QApplication(sys.argv)
         self.gui = GUI()
-        #self.controller = Controller()
+        # self.controller = Controller()
         self.params = []
         self.init_params()
-    
+
     def start(self):
         self.gui.show()
         self.gui.initialize(self)
         sys.exit(self.app.exec())
 
     def init_params(self):
-        self.params.append(Parameter(ParameterID.DELAY, "Delay", "s", True, 0, 100))
         self.params.append(Parameter(ParameterID.DC, "DC", "V", True, 0, 20))
         self.params.append(Parameter(ParameterID.AC, "AC", "V", True, 0, 20))
-        self.params.append(Parameter(ParameterID.PRESSURE, "Pressure", "mbar", True, 0, 1e-2))
-        self.params.append(Parameter(ParameterID.CURRENT, "Ion Current", "A", False, None, None))
+        self.params.append(Parameter(ParameterID.DELAY, "Delay", "s", True, 0, 100))
+        self.params.append(
+            Parameter(ParameterID.PRESSURE, "Pressure", "mbar", True, 0, 1e-2)
+        )
+        self.params.append(
+            Parameter(ParameterID.CURRENT, "Ion Current", "A", False, None, None)
+        )
 
 
 if __name__ == "__main__":
-   m = Main()
-   m.start()
+    m = Main()
+    m.start()
