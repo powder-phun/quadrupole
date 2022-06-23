37e135714313eb7746d6092b817f7422bde5c3f8
JJendryka <jendryka.j@gmail.com>
2022-06-23 14:17:07 +0200

    Working keithley controler

---
+++ controllers/keithleyVController.py
@@ -0,0 +1,65 @@
+import pyvisa
+import time
+
+from controllers.controller import Controller
+from parameter import ParameterID
+
+class KeithleyVController(Controller):
+    def __init__(self):
+        self.device = None
+
+        self.voltage = 0
+        
+
+    def getHandled(self):
+        return [
+            ParameterID.HV_DETECTOR,
+            ParameterID.CURRENT,
+        ]
+
+
+    def adjust(self, param: ParameterID, value: float) -> None:
+        if param == ParameterID.HV_DETECTOR:
+            self.voltage = value
+            self.device.write("B{},0,0X".format(int(value)))
+
+
+    def connect(self) -> bool:
+        if self.device is None:
+            rm = pyvisa.ResourceManager()
+            devices = rm.list_resources()
+            print(devices)
+            for device in devices:
+                if "GPIB" in device:
+                    print("Connecting to keithley")
+                    print(device)
+                    time.sleep(0.1)
+                    self.device = rm.open_resource(device)
+
+                    self.device.write("J0X") # Restore default settings
+                    self.device.write("N0X")  #Standby mode
+                    self.device.write("G4,2,0X") # Setup communication format
+                    self.device.write("F0,0X") # Setup DC V Bias
+                    self.device.write("L1,0X") # Setup auto range
+
+                    return True
+            else:
+                return False
+        else:
+            return True
+
+
+    def enable(self, state: bool):
+        if state:
+            self.device.write("N1X")
+            self.device.write("H0X")
+        else:
+            self.device.write("N0X")
+
+
+    def read(self, param: ParameterID) -> float:
+        if param == ParameterID.CURRENT:
+            val = float(self.device.read().strip())
+            return val
+        if param == ParameterID.HV_DETECTOR:
+            return self.voltage
