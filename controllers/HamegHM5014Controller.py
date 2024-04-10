52fd45bf8eba08b2d7198bd3ad2caef9e8f8fd73
powder-phun <powder-phun@users.noreply.github.com>
2024-04-10 09:43:18 +0200

    Work on Hameg SA controller

--- controllers/HamegHM5014Controller.py
+++ controllers/HamegHM5014Controller.py
@@ -5,13 +5,13 @@ import pyvisa
 import serial
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy as sp
 
 from controllers.controller import Controller
 from config import ParamConfig
 from config import ControllerConfig
 from time import sleep
 from device import Device
-
 
 class HamegHM5014Controller(Controller):
     def __init__(self, config):
@@ -51,6 +51,7 @@ class HamegHM5014Controller(Controller):
         return {}
 
     def parseConfig(self):
+        print(self.config.json)
         if "port" in self.config.json:
             self.serial_port = self.config.json["port"]
         else:
@@ -84,18 +85,21 @@ class HamegHM5014Controller(Controller):
         self.ser.write(b'#kl1\r')
         self.ser.write(b'#br115200\r')
         self.ser.close()
-        time.sleep(1)
+        time.sleep(0.1)
         self.ser = serial.Serial(self.serial_port, 115200)
-
+        time.sleep(0.1)
         self.ser.write(b'#kl1\r')
-
+        time.sleep(0.1)
         if self.center_freq is not None:
             self.ser.write("#cf{:08.3f}\r".format(self.center_freq).encode())
             print("#cf{:08.3f}\r".format(self.center_freq).encode())
+            time.sleep(0.1)
         if self.span is not None:
-            self.ser.write((f"#cf{self.span}\r").encode())
+            self.ser.write((f"#sp{int(self.span)}\r").encode())
+            print((f"#sp{int(self.span)}\r").encode())
+            time.sleep(0.1)
         if self.config_commands is not None:
-            self.ser.write(self.config_commands.encode())
+            pass#self.ser.write(self.config_commands.encode())
         print("sent")
         return True
 
@@ -106,19 +110,52 @@ class HamegHM5014Controller(Controller):
 
 
     def read(self, param: str) -> float:
-        print("read")
         if param == "Amax":
+            self.ser.write((f"#dm0\r").encode())
+            time.sleep(0.1)
+            self.ser.write((f"#vm3\r").encode())
+            time.sleep(6)
+            
             self.ser.flushInput()
             self.ser.write((f"#bm1\r").encode())
             c = self.ser.read(5)
             c = self.ser.read(2001)
+            
+            
+            amplitudes = np.frombuffer(c, dtype=np.uint8)*0.4-80.8
+            frequencies = np.linspace(self.center_freq-self.span/2, self.center_freq+self.span/2, 2001)
+            lin_amplitudes = 10**(amplitudes/10) * 1e-3
+            print(amplitudes)
+            
+            peaks, properties = sp.signal.find_peaks(amplitudes, height=-56, threshold=None, distance=None, prominence=None, width=None, wlen=None, rel_height=0.5, plateau_size=None)
 
-            print(c)
-            np_array = np.frombuffer(c, dtype=np.uint8)
-            #plt.plot(np_array)
-            #plt.show()
+            plt.plot(frequencies, amplitudes)
+            plt.plot(frequencies[peaks], amplitudes[peaks], "x")
+            plt.show()
+
+            plt.plot(frequencies, lin_amplitudes)
+            plt.plot(frequencies[peaks], lin_amplitudes[peaks], "x")
+            plt.show()
+
+            for peak in peaks:
+                print(frequencies[peak], lin_amplitudes[peak])
+
+
+            lin_amplitudes[np.argmax(lin_amplitudes)] = 0
+
+            print(lin_amplitudes[peaks])
+            
+            lin_Amax = np.max(lin_amplitudes) #in watts
+            lin_thd = np.sum(lin_amplitudes[peaks]) / lin_Amax #in terms of power
+            Amax = 10 * np.log10(lin_Amax/1e-3)
+            thd = 10 * np.log10(lin_thd)
+
+
+            print(f"THD {lin_thd}, Amax {lin_Amax}w")
+            print(f"THD {thd}dB, Amax {Amax}dBm")
+
 
             print(self.ser.in_waiting)
-            return(np.max(np_array))
+            return(np.max(amplitudes))
         else:
             logging.error("Wrong param name")
---
+++ hameg_test.py
@@ -0,0 +1,12 @@
+from controllers.HamegHM5014Controller import HamegHM5014Controller
+from config import Config
+import time
+
+config = Config('/home/jan/code/quadrupole/configs/HM5014.json')
+print(config.controllers)
+
+hm = HamegHM5014Controller(config.controllers[0])
+
+hm.connect()
+time.sleep(1)
+hm.read("Amax")
